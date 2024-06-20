d91afd891c40881b5daaa77b779ce50e6143bcc2
Tonyxi16 <xlh505440@163.com>
2024-06-20 20:32:43 +0800

    create cloudalibaba-naocs-config-client3378

---
+++ cloudalibaba-naocs-config-client-3378/src/main/java/com/leslie/springcloud_demo/alibaba/NacosConfigClientMain3378.java
@@ -0,0 +1,18 @@
+package com.leslie.springcloud_demo.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+// 扫描 Swagger2Config
+@ComponentScan("com.leslie")
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3378 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3378.class,args);
+        System.out.println();
+        System.out.println("http://localhost:3378/swagger-ui.htm");
+    }
+}
